2efad9965f847ded104e6e5c973faff9cb8e5f9d
Jake Ascher <ascherj@gmail.com>
2024-02-22 16:53:51 -0700

    Get the latest 10 events for the user

--- main.py
+++ main.py
@@ -4,20 +4,32 @@ from dotenv import load_dotenv
 import gitlab
 
 class App():
+
     def __init__(self, token: str) -> None:
         self.token = token
+        self.gl = None
+        self.user = None
+        self.events = None
 
     def establish_connection(self) -> None:
         self.gl = gitlab.Gitlab(private_token=self.token)
 
     def authenticate(self) -> None:
         self.gl.auth()
-        user = self.gl.user
-        print(f"Authenticated as {user.name} ({user.username})")
+        self.user = self.gl.user
+        print(f"Authenticated as {self.user.name} ({self.user.username})")
+
+    def get_user_events(self) -> None:
+        events = self.gl.events.list(per_page=10)
+        print(f"Found {len(events)} events")
+        events_as_dicts = [event.attributes for event in events]
+        self.events = events_as_dicts
 
     def run(self) -> None:
         self.establish_connection()
         self.authenticate()
+        self.get_user_events()
+        print(self.events)
 
 
 if __name__ == "__main__":
