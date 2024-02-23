7e01835714546a501d44b6abf8fd41d0faec3e9d
Jake Ascher <ascherj@gmail.com>
2024-02-23 16:27:40 -0700

    Filter events and add method documentation
    "left" and "deleted" events, such as leaving a project or deleting a
    branch, are filtered out of the events list. Also, method documentation
    was added.

--- main.py
+++ main.py
@@ -12,24 +12,41 @@ class App():
         self.events = None
 
     def establish_connection(self) -> None:
+        """
+        Establishes a connection to the GitLab API using the private token.
+        """
         self.gl = gitlab.Gitlab(private_token=self.token)
 
     def authenticate(self) -> None:
+        """
+        Authenticates the user and sets the user attribute to the authenticated user.
+        """
         self.gl.auth()
         self.user = self.gl.user
         print(f"Authenticated as {self.user.name} ({self.user.username})")
 
-    def get_user_events(self) -> None:
-        events = self.gl.events.list(per_page=10)
+    def get_and_filter_user_events(self) -> None:
+        """
+        Gets events for the user and filters out "left" and "deleted" events
+        as those are not likely counted as "contributions" in GitHub.
+        """
+        events = self.gl.events.list(per_page=100)
         print(f"Found {len(events)} events")
         events_as_dicts = [event.attributes for event in events]
-        self.events = events_as_dicts
+        print(events_as_dicts)
+        filtered_events = [
+            event for event in events_as_dicts
+            if event["action_name"] != "left" and event["action_name"] != "deleted"
+        ]
+        print(f"Filtered down to {len(filtered_events)} events")
 
     def run(self) -> None:
+        """
+        Runs the application.
+        """
         self.establish_connection()
         self.authenticate()
-        self.get_user_events()
-        print(self.events)
+        self.get_and_filter_user_events()
 
 
 if __name__ == "__main__":
